07ac54d566934ee18f9d34cf2083b412505ad120
Frances Hansen <francesjuniperhansen@gmail.com>
2020-01-31 08:49:02 -0800

    created universities array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -59,7 +59,9 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map((university)=>{
+  return {'university': university.university} //better than starting with empty array!
+});
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
