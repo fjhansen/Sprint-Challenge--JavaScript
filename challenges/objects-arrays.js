8a8dfd4a9cbfefb4f443f371ec0c2b43a939af0c
Frances Hansen <francesjuniperhansen@gmail.com>
2020-01-31 09:48:36 -0800

    lowCaseAnimales complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -139,7 +139,12 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((animalName)=>{
+  animalName.animal_name = animalName.animal_name.toLowerCase();
+    return animalName.animal_name
+
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
